15f84f3814d347923b48e43fe485dd924393fbc1
Anas Ben Mustafa <57250290+arabianprince23@users.noreply.github.com>
2021-05-25 14:20:10 +0300

    added lab 14

---
+++ 14-2.c
@@ -0,0 +1,24 @@
+#include <signal.h>
+#include <stdio.h>
+
+void my_handler(int nsig) {
+    if(nsig == SIGQUIT){
+        printf("CTRL-4 just tapped");
+    }
+    else if(nsig == SIGINT){
+        printf("CTRL-C just tapped");
+    }
+}
+
+int main(void) {
+    //
+    // Set the process response to the SIGINT signal
+    //
+    (void)signal(SIGINT, my_handler);
+    (void)signal(SIGQUIT, my_handler);
+    //
+    // From this point, the process will print SIGINT message.
+    //
+    while(1);
+    return 0;
+}
